de78c00033c3d897dd24f5762f19d14c7388a266
Syed Aman <sna274@nyu.edu>
2018-04-03 16:34:49 -0500

    setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-document.getElementById("app").innerHTML = `
+document.getElementById(
+  'app',
+).innerHTML = `
 <h1>Hello Parcel!</h1>
 <div>
   Look
@@ -6,3 +8,15 @@ document.getElementById("app").innerHTML = `
   for more info about Parcel.
 </div>
 `;
+
+const rootElement = document.getElementById(
+  'app',
+);
+console.log(rootElement);
+// Create a 3 x 3 board.
+// Add in click handler to every tile.
+// First click should make a X on the tile.
+// Second click sohuld make an O on the tile.
+// Repeat until all tiles are filled...
+// Display game over on fill.
+// Display button to start over.
